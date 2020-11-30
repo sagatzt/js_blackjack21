ce766d3cd8e9ff62208f1bb0602ee6022ec0d85d
Jose Luis Bch <sagatzt@gmail.com>
2020-11-30 10:41:26 +0100

    cambios aprobados

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-let carta = document.querySelector("#carta")
-let baraja = document.querySelector("#baraja")
+let jugador=document.querySelector("input[name='jugador']")
+let imgCarta = document.querySelector("#img-carta")
+let imgJugada=document.querySelector(".img-jugada")
 let modal = document.querySelector(".modal")
-let jugadaCartas=document.querySelector(".jugada-cartas")
+let alert = document.querySelector(".alert")
 
 let palos = ["corazones","picas","treboles","diamantes"]
-let cartas=[],jugada=[], puntos=0
+let cartas=[],jugada=[]
 
-//boton modal reiniciar
-modal.querySelector("button[name='btnReiniciar']").onclick=()=>{
-    console.log("hola")
-    modal.classList.add("modal-hidden")
-    reiniciarPartida()
-}
 //boton sacar carta
 document.querySelector("#btnSacarCarta").onclick=sacarCarta
 //boton me planto
 document.querySelector("#btnMePlanto").onclick=()=>{
     document.querySelector("#btnSacarCarta").setAttribute("disabled",true)
+    let puntos=jugada.map(c=>c.valor).reduce((a,b)=>a+b)
+    if(puntos+cartas[0].valor<=21)
+        mostrarMensaje(`Sabía que eras un cobarde. El siguiente valor era ${cartas[0].valor}. Tu pierdes.`)
+    else
+        mostrarMensaje(`Esta vez te ha salido bien. El siguiente valor era ${cartas[0].valor}. Tu ganas.`)
 }
+//link del menu de navegacion: reiniciar
+document.querySelector("#lnkReiniciar").onclick=reiniciarPartida
 
 
+/////////////////////////////////////////////////////
 //comienza el juego
 mezclarCartas()
 
@@ -28,34 +31,52 @@ function mezclarCartas(){
         for(let j=1;j<14;j++)
             cartas.push({palo: palo, valor: j>10?10:j, imagen: palo+"_"+j+".svg"})
     })
-    cartas.sort(()=>Math.random()-0.5)
+    cartas=cartas.sort(()=>Math.random()-0.5)
     sacarCarta()
 }
 
 function sacarCarta(){
-    carta.src="./images/baraja/" + cartas[0].imagen
-    let miniCarta=carta.cloneNode()
+    jugada.push(cartas.shift())
+    imgCarta.src="./images/baraja/" + jugada[jugada.length-1].imagen
+    let miniCarta=imgCarta.cloneNode()
     miniCarta.classList.add("mini")
-    puntos+=cartas[0].valor
-    document.querySelector("#puntos").textContent=puntos
-    jugadaCartas.appendChild(miniCarta)
-    cartas.shift()
-    if(cartas.length==0) mezclarCartas()
+    imgJugada.appendChild(miniCarta)
     comprobarJugada()
 }
 
 function comprobarJugada(){
-    if(puntos>=21){
-        let res=puntos==21?"HAS GANADO":"HAS PERDIDO"
-        modal.querySelector("#resultado").textContent=res    
-        modal.classList.remove("modal-hidden")
-    }
+    let puntos=jugada.map(c=>c.valor).reduce((a,b)=>a+b)
+    document.querySelector("#puntos").textContent=puntos
+    if(cartas.length==0) mezclarCartas()
+    if(puntos==21)
+        mostrarMensaje(`${puntos} puntos. Enhorabuena! por fin ganas una.`)
+    else if(puntos>21)
+        mostrarMensaje(`${puntos} puntos. Eres un paquete.`) 
+    else{
+        //mostrar una frase de animo
+    }   
 }
 
 function reiniciarPartida(){
-    while(jugadaCartas.firstChild)
-        jugadaCartas.removeChild(jugadaCartas.firstChild)
-    puntos=0
+    //if(confirm("¿Deseas guardar esta partida?")) guardarPartida()
+    while(imgJugada.firstChild)
+        imgJugada.removeChild(imgJugada.firstChild)
+    jugada=[]
+    document.querySelector("#btnSacarCarta").removeAttribute("disabled")
+    alert.classList.remove("mostrar")
     mezclarCartas()
 }
 
+function guardarPartida(){
+    let jugadasDe=JSON.parse(localStorage.getItem(jugador.value))
+    if(jugadasDe==null) jugadasDe=[]
+    jugadasDe.push(jugada)
+    localStorage.setItem(jugador.value,JSON.stringify(jugadasDe))
+}
+
+// Con esta función se muestra el mensaje 
+function mostrarMensaje(texto) {
+    alert.querySelector(".texto").innerHTML=texto
+    alert.classList.add("mostrar")
+}
+alert.querySelector(".close").onclick=()=>alert.classList.remove("mostrar")
